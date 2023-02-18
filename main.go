c2b3cb3cc6f89f7804f16dd715d1132094577809
chinathaip <cartoonabe@gmail.com>
2023-02-18 14:42:14 +0700

    twosum linear time

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ func main() {
 	// x := containsDuplicate([]int{1, 2, 3, 1})
 	// x := containsDuplicate([]int{1, 1, 1, 3, 3, 4, 3, 2, 4, 2})
 
-	x := maxSubArray([]int{-2, 1, -3, 4, -1, 2, 1, -5, 4})
+	// x := maxSubArray([]int{-2, 1, -3, 4, -1, 2, 1, -5, 4})
+
+	x := twoSum([]int{2, 7, 11, 15}, 9)
 	fmt.Println(x)
 }
--- twoSum.go
+++ twoSum.go
@@ -1,21 +1,37 @@
 package main
 
 /*
-1. nested loop through the slice
-2. see if the current num + the rest of num matches with target or not
-3. yes -> return index of current num and that number
+ 1. loop through the slice
+ 2. get the complement by doing target - current num
+ 3. check if the map already contains the complement -->
+    3.1 if yes -> return the index of the complement and the index of current num
+    3.2 if not contain -> add to complement to the map along with its index
+
+[2,7,11,15]
+
+9-2 = 7
+9-7 = 2
+9-11 = -2
+9-15 = -6
+
+	{
+		7: 0
+		2: 1
+		-2:2
+		-6:3
+	}
 */
 func twoSum(nums []int, target int) []int {
-	arr := []int{}
-	for i := 0; i < len(nums); i++ {
-		for j := i + 1; j < len(nums); j++ {
-			if nums[i]+nums[j] == target {
-				arr = append(arr, i, j)
-			}
+	numToIndex := make(map[int]int, len(nums))
+	for i, num := range nums {
+		complement := target - num
+		if complementIndex, found := numToIndex[complement]; found {
+			return []int{complementIndex, i}
 		}
+		numToIndex[num] = i
 	}
-	return arr
+	return nil
 }
 
-//Time complexity: O(n^2)
-//Space complextiy: O(1)
+//Time complexity: O(n)
+//Space complextiy: O(n)
